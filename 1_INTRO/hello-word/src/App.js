d4721c8aff793ced3c6327b27bb253094b9d0693
tjeveraldo <everaldogomesbs@gmail.com>
2022-09-28 15:10:21 -0300

    primeira subida

---
+++ 1_INTRO/hello-word/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <h2>emmet funfou </h2>
+      <p>Meu primeiro parágrafo em React</p>
+    </div>
+  );
+}
+
+export default App;
+
