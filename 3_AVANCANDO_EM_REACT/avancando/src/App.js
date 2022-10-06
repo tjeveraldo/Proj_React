82799f43bf82d3fb4040b0dd7978be15912310f8
tjeveraldo <everaldogomesbs@gmail.com>
2022-10-06 13:41:26 -0300

    conhecendo props

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,12 +1,17 @@
 
+import { useState } from 'react';
 import './App.css';
 
 import City from './assets/city.jpg'
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const name="Joaquim";
+
+  const [userName] = useState("Manoel")
   return (
     <div className="App">
       <h1>Oba, estou avançando em React</h1>
@@ -21,6 +26,7 @@ function App() {
       <ManageData/>
       <ListRender/>
       <ConditionalRender/>
+      <ShowUserName name={userName}/>
     </div>
   );
 }
